28ab1295d438bf92d23c8565bbca5c8f82b907ea
maryschmidtdev <68038839+maryschmidtdev@users.noreply.github.com>
2020-08-07 16:05:41 -0500

    Add files via upload
    Initial Upload

---
+++ DogApiBreedRequest/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function getDogImage() {
+  const breed = $("#breed-name").val()
+  //console.log(breed)
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+ 
+  //replace the existing image with the new one
+
+    
+    $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
