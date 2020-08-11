04ca1ef715a8f4484b21a487769c287f23f78dde
maryschmidtdev <68038839+maryschmidtdev@users.noreply.github.com>
2020-08-11 18:29:11 -0500

    revisions request
    revisions updated - error message

--- DogApiBreedRequest/index.js
+++ DogApiBreedRequest/index.js
@@ -1,36 +1,39 @@
-'use strict';
+"use strict";
 
 function getDogImage() {
-  const breed = $("#breed-name").val()
+  const breed = $("#breed-name").val();
   //console.log(breed)
   fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .then((response) => response.json())
+    .then((responseJson) => displayResults(responseJson))
+    .catch((error) =>
+      alert(
+        "Please verify the name and spelling of the breed you are searching for."
+      )
+    );
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
- 
-  //replace the existing image with the new one
 
-    
-    $('.results-img').replaceWith(
+  //replace the existing image with the new one
+  $(".results-img").slice(1).remove();
+
+  $(".results-img").replaceWith(
     `<img src="${responseJson.message}" class="results-img">`
-  )
+  );
   //display the results section
-  $('.results').removeClass('hidden');
+  $(".results").removeClass("hidden");
 }
 
 function watchForm() {
-  $('form').submit(event => {
+  $("form").submit((event) => {
     event.preventDefault();
     getDogImage();
   });
 }
 
-$(function() {
-  console.log('App loaded! Waiting for submit!');
+$(function () {
+  console.log("App loaded! Waiting for submit!");
   watchForm();
 });
